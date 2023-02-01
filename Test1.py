1a3013633c3cfdadbb3339316ea8b57de4de2d26
darkob-code <87665820+darkob-code@users.noreply.github.com>
2023-02-01 01:22:45 +0100

    Add files via upload

---
+++ Test1.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+os.environ['PATH'] += "c:\Selenium_Driver" #putanja do webdrivera, svaki browser ima svoj driver kojeg treba skinuti i treba paziti na verziju browsera, mora biti ista kao driver
+driver = webdriver.Chrome()
+
+driver.get('https://darkob-code.github.io/Selenium_Python_Pycharm_Sum_two_fields/')
+sum1 = driver.find_element(By.NAME, 'firstBox')
+sum2 = driver.find_element(By.NAME, 'secondBox')
+
+sum1.send_keys(15) #automatizacija upisa broja u polje
+sum2.send_keys(15) #automatizacija upisa broja u polje
